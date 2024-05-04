56e98c9e0b29a10a23ecc9bd21c20d3b292fb7a0
Kazbegi Kachmazovi <1kachmazovi@gmail.com>
2024-05-04 18:42:52 +0400

    add styles and inputs for register component

--- src/app/register-login/register/register.component.ts
+++ src/app/register-login/register/register.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { passwordMatchValidator } from './validators';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  public userForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    surname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    passwordGroup: new FormGroup(
+      {
+        password: new FormControl('', [
+          Validators.required,
+          Validators.pattern(
+            '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^ws]).{8,}$'
+          ),
+        ]),
+        confirmPassword: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6),
+        ]),
+      },
+      [passwordMatchValidator()]
+    ),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+    }),
+    terms: new FormControl(false, [Validators.requiredTrue]),
+  });
 
+  public hasFormControlError(name: string): boolean {
+    return (
+      (this.userForm.get(name)?.invalid && this.userForm.get(name)?.touched) ||
+      false
+    );
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,24 +5,21 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MyInputDirective } from './directives/my-input.directive';
 import { MyBtnDirective } from './directives/my-btn.directive';
-
-
+import { ErrMsgComponent } from './components/err-msg/err-msg.component';
 
 @NgModule({
   declarations: [
     PageNotFoundComponent,
     MyInputDirective,
-    MyBtnDirective
+    MyBtnDirective,
+    ErrMsgComponent,
   ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   exports: [
     ReactiveFormsModule,
     MyInputDirective,
-    MyBtnDirective
-  ]
+    MyBtnDirective,
+    ErrMsgComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
